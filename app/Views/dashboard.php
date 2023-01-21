c5f4a5ec17f8f1582b68276eaaa816d2ea23b35e
fachrurrozi5 <rozifachrur511@gmail.com>
2023-01-21 23:06:16 +0700

    uas

---
+++ app/Views/dashboard.php
@@ -0,0 +1,59 @@
+<?php
+$session = \Config\Services ::session();
+$user = $session->get('username');
+?>
+<?= $this->include('header'); ?>
+<?= $this->include('sidemenu'); ?>
+
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->
+
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <div class="row">
+                <div class="col-lg-12 col-md-4 order-1">
+                  <div class="row">
+                    <div class="col-lg-6 col-md-12 col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img
+                                src="<?= base_url('');?>/assets/img/icons/unicons/chart-success.png"
+                                alt="chart success"
+                                class="rounded"
+                              />
+                            </div>
+                          </div>
+                          <span class="fw-semibold d-block mb-1">Profit</span>
+                          <h3 class="card-title mb-2">$12,628</h3>
+                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-6 mb-4">
+                      <div class="card">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="avatar flex-shrink-0">
+                              <img
+                                src="<?= base_url('');?>/assets/img/icons/unicons/wallet-info.png"
+                                alt="Credit Card"
+                                class="rounded"
+                              />
+                            </div>
+                          </div>
+                          <span>Sales</span>
+                          <h3 class="card-title text-nowrap mb-1">$4,679</h3>
+                          <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- / Content -->
+    
+<?= $this->include('footer'); ?>
---
+++ app/Views/grafik_tipologi.php
@@ -0,0 +1,71 @@
+<?= $this->include('header'); ?>
+<?= $this->include('sidemenu'); ?>
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->
+            <div>
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <div class="row">
+              <center><h3 class="title"><b> GRAFIK TIPOLOGI </b></h3></center>
+                <!-- Total Revenue -->
+                <div class="col-12 col-lg-6 order-2 order-md-3 order-lg-2 mb-4">
+                  <div class="card">
+                    <div class="row row-bordered g-0">
+                      <div class="col-md-8">
+                        <h5 class="card-header m-0 me-2 pb-3">Grafik Pie</h5>
+                      </div>
+                      <div id="chartdiv"></div>
+                    </div>
+                  </div>
+                </div>
+                <!--/ Total Revenue -->
+                <!-- Total Revenue -->
+                <div class="col-12 col-lg-6 order-2 order-md-3 order-lg-2 mb-4">
+                  <div class="card">
+                    <div class="row row-bordered g-0">
+                      <div class="col-md-8">
+                        <h5 class="card-header m-0 me-2 pb-3">Grafik Column Bar</h5>
+                      </div>
+                      <div id="chartdiv"></div>
+                    </div>
+                  </div>
+                </div>
+                <!--/ Total Revenue -->
+              </div>
+            </div>
+            <!-- / Content -->
+
+<?= $this->include('footer'); ?>
+
+<script src="//www.amcharts.com/lib/4/core.js"></script>
+<script src="//www.amcharts.com/lib/4/charts.js"></script>
+
+
+<style>
+#chartdiv {
+  width: 100%;
+  height: 400px;
+}
+</style>
+<script>
+// Create chart instance in one go
+var chart = am4core.createFromConfig({
+  // Create pie series
+  "series": [{
+    "type": "PieSeries",
+    "dataFields": {
+      "value": "jumlah_masjid",
+      "category": "tipologi"
+    }
+  }],
+
+  // Add data
+  
+  "data":  <?php echo file_get_contents(base_url('grafik_tipologi/grafik_tipologi_json')); ?>,
+
+  // And, for a good measure, let's add a legend
+  "legend": {}
+
+}, "chartdiv", am4charts.PieChart);
+</script>
